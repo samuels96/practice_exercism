61ab5c35f2f5aedab8142e5828366d4550504cf7
samuels96 <37853844+samuels96@users.noreply.github.com>
2018-04-01 15:14:11 -0400

    exercism practice

---
+++ hamming/hamming.py
@@ -0,0 +1,11 @@
+def distance(strand_a, strand_b):
+	if len(strand_a) != len(strand_b):
+		raise ValueError("Inputs not identical in length")
+	count = 0
+	for a,b in zip(strand_a, strand_b):
+		if a != b:
+			count+=1
+	return count
+
+   
+print (distance("AT", "CT"))
